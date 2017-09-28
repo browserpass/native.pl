b872020ec7ec0903755e9792928a4a2ccd5a547f
Christoph Böhmwalder <Christoph.Boehmwalder@catalysts.cc>
2017-09-28 09:28:56 +0200

    add helpful usage message
    and a few command line switches

--- README.md
+++ README.md
@@ -1,24 +1,47 @@
 # native.pl
 
-This script enables you to test your [Native Messaging](https://developer.chrome.com/apps/nativeMessaging) setup by allowing you to send arbitrary commands to your executable and to the response in plain old text.
+This script enables you to test your [Native Messaging](https://developer.chrome.com/apps/nativeMessaging) setup by allowing you to send arbitrary commands to your executable and to see the response in plain old text.
 
 ## Usage
 ```
-$ ./native.pl [EXECUTABLE] [COMMAND]...
+native.pl [options] -- host_binary [command]
+
+    Options:
+      -h, --help      Display a help message
+      -t, --time      Enable output of timing information
+      -               Read from stdin instead of argument list
 ```
 
 Where:
-* `EXECUTABLE` is a path to your host executable
-* `COMMAND` is the string that will be sent to your host (**can** contain spaces)
+* `host_binary` is a path to your host executable
+* `--` separates options from parameters
+* `command` is the string that will be sent to your host (**can** contain spaces)
+
+**Note:** When reading from stdin, currently only the first line is interpreted.
 
 ## Example
 
 *(Using [browserpass](https://github.com/dannyvankooten/browserpass))*
+
 
 ```
 $ ./native.pl ~/browserpass/browserpass {\"action\": \"search\", \"domain\": \"github.com\"}
 Request: |  {"action": "search", "domain": "github.com"}
 Result:  |  ["github.com\\chrboe"]
 Errors:  |  -
-Time:    |  0.062s
+```
+
+Or reading from stdin (notice how only the first line is evaluated):
+
+```
+$ cat commands.txt
+{"action": "search", "domain": "github.com"}
+{"action": "search", "domain": "google.com"}
+{"action": "search", "domain": "facebook.com"}
+{"action": "search", "domain": "twitter.com"}
+
+$ ./native.pl ~/browserpass/browserpass.exe < test.txt
+Request: |  {"action": "search", "domain": "google.com"}
+Result:  |  ["github.com\\chrboe"]
+Errors:  |  -
 ```
--- native.pl
+++ native.pl
@@ -2,19 +2,43 @@
 
 # native.pl - A perl client for testing Chrome Native Messaging.
 # Copyright (C) 2017  Christoph Böhmwalder
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=encoding utf8
+=head1 NAME
+
+native.pl - A perl client for testing Chrome Native Messaging
+
+=head1 SYNOPSIS
+
+native.pl [options] -- host_binary [command]
+
+    Options:
+      -h, --help      Display this help message
+      -t, --time      Enable output of timing information
+      -               Read from stdin instead of argument list
+
+=head1 LICENSE
+
+This is licensed under the GNU General Public License v3
+
+=head1 AUTHOR
+
+Christoph Böhmwalder
+
+=cut
 
 use strict;
 use warnings;
@@ -28,10 +52,41 @@ use feature qw(switch);
 use Term::ANSIColor;
 use Term::ANSIColor qw(:constants);
 use Time::HiRes qw ( time alarm sleep );
+use Pod::Usage;
+use Getopt::Long;
+
+
+# get command line options
+my $stdio = "";
+my $help = "";
+my $time = "";
+
+GetOptions (
+    "" => \$stdio,
+    "help|h" => \$help,
+    "time|t" => \$time
+);
+
+pod2usage(0)  if ($help);
 
 # check for arguments
-# TODO: usage information
-die "Invalid number of arguments" if $#ARGV+1 < 1;
+pod2usage("$0: Invalid number of arguments.\n")  if (@ARGV < 1);
+
+
+sub get_text {
+    # if there are no arguments, assume input over stdin
+    if ($stdio || @ARGV < 1) {
+        # read one line from stdin
+        my $line = <STDIN>;
+
+        # trim whitespace
+        $line =~ s/^\s+|\s+$//g;
+
+        return $line;
+    } else {
+        return join("", @ARGV);
+    }
+}
 
 my ($cmd_in, $cmd_out, $cmd_err);
 $cmd_err = gensym;
@@ -39,15 +94,12 @@ $cmd_err = gensym;
 # open specified program for piping
 my $pid = open3($cmd_in, $cmd_out, $cmd_err, shift);
 
-# check arguments again
-die "No input" if $#ARGV+1 < 1;
-
 # get text from arguments and calculate length
-my $text = join("", @ARGV);
+my $text = get_text();
 my $len = length $text;
 
 # save the start time here
-my $start = time;
+my $start = time if ($time);
 
 # write the length to the pipe (little endian format)
 print $cmd_in pack("L<", $len) . $text;
@@ -87,10 +139,10 @@ waitpid($pid, 0);
 # trim length bytes from response
 $stdout_output = substr($stdout_output, 4) unless $stdout_output eq "";
 
-my $duration = sprintf("%.3f", time - $start);
+my $duration = sprintf("%.3f", time - $start) if ($time);
 
 # print results
 print YELLOW, "Request:", RESET, " |  ", CYAN, $text, RESET, "\n";
 print YELLOW, "Result:", RESET, "  |  ", GREEN, ($stdout_output or "-\n"), RESET;
 print YELLOW, "Errors:", RESET, "  |  ", RED, ($stderr_output or "-\n"), RESET;
-print YELLOW, "Time:", RESET, "    |  ", $duration, "s\n";
+print YELLOW, "Time:", RESET, "    |  ", $duration, "s\n" if ($time);
